d826d8bb31a13121484ab61606f1606e575753ca
cboyd74 <crboyd@wisc.edu>
2021-03-25 11:59:34 -0500

    added redirection to LogIn.js

--- HomeworkHub-version1.0.0/src/views/LogIn.js
+++ HomeworkHub-version1.0.0/src/views/LogIn.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import  { Redirect } from 'react-router-dom'
+import  { Redirect } from 'react-router-dom';
 import axios from "axios";
-import { 
-         FormGroup,
+import { FormGroup,
          FormControl,
          } from "react-bootstrap";
 
@@ -14,9 +13,9 @@ export default class LogIn extends Component {
     this.state = {
       username: "",
       password: "",
+      redirect: null,
       wrongPass: false,
     };
-
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -27,13 +26,17 @@ export default class LogIn extends Component {
     formData.append("formData", data);
     axios.post(url, formData)
     .then(response => {
-
         var res = response["data"];
-        //Need to add else if ("parent") when implementing parent stuff
+        //Need to add else if ("parent") when implementing parent side
         if (res == "teacher") {
           console.log("teacher user found");
-          //Redirect
+          this.setState({
+            redirect: "/admin/dashboard",
+          });
         } else {
+          this.setState({
+            wrongPass: true,
+          });
           console.log("user not found")
         }
     })
@@ -47,6 +50,9 @@ export default class LogIn extends Component {
   };
 
   render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />
+    }
  
     return ( 
       <div className="loginpage">
@@ -72,12 +78,15 @@ export default class LogIn extends Component {
             />
           </FormGroup>
 
+          { this.state.wrongPass ? <h6>Username or password incorrect</h6> : null }
+
           <h5>Don't have an account? <a href="/signup" > Register Now</a></h5>
 
           <button
             block
             size="lg"
-            onClick = {this.handleLoginButtonClick}>Login
+            onClick = {this.handleLoginButtonClick}>
+              Login
           </button>
       </div>
     );
