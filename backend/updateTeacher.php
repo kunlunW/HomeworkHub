e26c0c3751ded32ffc4ed4b0280b6ce591dd99d9
EVEBios <61592111+EVEBios@users.noreply.github.com>
2021-04-06 05:52:15 +0800

    Added Parents/Teachers Info Update

---
+++ backend/updateTeacher.php
@@ -0,0 +1,43 @@
+<?php
+ include "sql_connection.php";
+
+// Create connection
+$conn = OpenCon();
+
+// Retrieve data from the frontend
+$data = $_POST['formData'];
+$json = json_decode($data, true);
+$username = $json["username"]; // Primary Key
+
+$gender = $json["gender"];
+$subject = $json["subject"];
+$address = $json["address"];
+$telephone = $json["telephone"];
+
+$sqlCheck = "SELECT * FROM Teachers WHERE teacherUserName = '$username';";
+if($sqlCheck) { // Entry already exists
+    $sqlUpdate = "UPDATE Teachers t SET t.gender = '$gender', t.subject = '$subject', t.address = '$address', t.telephone = '$telephone' WHERE t.teacherUserName = '$username';";
+    $updateRes = $conn->query($sqlUpdate);
+    if (!$updateRes) {
+        echo "Update Failed\n";
+        CloseCon($conn);
+        return;
+    } else {
+        echo "Update Success\n";
+    }
+} else { // Entry doesn't exist
+    $sqlInsert = "INSERT INTO Teachers (username, gender, subject, address, telephone) VALUES ('$username', '$gender', '$subject', '$address', '$telephone');";
+    $insertRes = $conn->query($sqlInsert);
+    if (!$insertRes) {
+        echo "Insert Failed\n";
+        CloseCon($conn);
+        return;
+    } else {
+        echo "Insert Success\n";
+    }
+}
+
+CloseCon($conn);
+?>
+
+
