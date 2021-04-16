f5fb874d01fb2ee03c15c3bf13ac612e4487db1c
Cameron Boyd <crboyd@wisc.edu>
2021-04-16 16:52:39 -0500

    Edit profile fully connected, create classroom fixed

--- HomeworkHub-version1.0.0/src/views/UserProfile.js
+++ HomeworkHub-version1.0.0/src/views/UserProfile.js
@@ -46,29 +46,33 @@ class User extends React.Component {
 
   }
 
-  updateFields(values) { 
-    this.setState({
-      gender: values.gender,
-      email: values.email,
-      mobile_no: values.mobile_no,
-      school: values.school
-    });
-    
-    const url = "/HomeworkHub/backend/updateTeacher.php";
+  updateFields(values) {   
+      var ret;
+      const url = "/HomeworkHub/backend/updateTeacher.php";
       let formData = new FormData();
-      let data = '{"username":"' + this.state.name + '", "gender":"' + this.state.gender 
-                                 + '", "email":"' + this.state.email 
-                                 + '", "mobile_no":"' + this.state.mobile_no 
-                                 + '", "school":"' + this.state.school + '"}';          
+      let data = '{"username":"' + values.name + '", "gender":"' + values.gender 
+                                 + '", "email":"' + values.email 
+                                 + '", "mobile_no":"' + values.mobile_no 
+                                 + '", "school":"' + values.school + '"}';          
       formData.append("formData", data);
       console.log(data);
       axios.post(url, formData)
       .then(response => {
         var res = response["data"];
-        console.log(res);    
+        console.log("res: " +res);    
+        ret = res
       })
       .catch(err=>console.log(err.response, err.request));
 
+      //if insert is successful, set state
+      if (ret === 1 || ret === 3) {
+        this.setState({
+          gender: values.gender,
+          email: values.email,
+          mobile_no: values.mobile_no,
+          school: values.school
+        });
+      }
   }
 
   render() {
