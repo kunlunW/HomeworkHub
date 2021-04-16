ed17702bb104099f359200f5a3ae8ef59ba2c6d6
kunlunW <52982585+kunlunW@users.noreply.github.com>
2021-04-16 17:45:10 -0500

    Add files via upload
    calendar view update

---
+++ HomeworkHub-version1.0.0/src/views/components_calendar/Title/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const DivWrapper = styled('div')`
+  background-color: #2A2B2D;
+	height: 0px;
+`;
+
+const Title = () => (
+	<DivWrapper>
+        
+    </DivWrapper>
+);
+
+export { Title };
